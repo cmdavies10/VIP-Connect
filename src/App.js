b7af7beee7c319ea6e619c8964c909595bc06146
Chris Davies <c.m.davies10@gmail.com>
2019-12-04 13:51:04 -0500

    created components folder, added Validate component and configured within App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import Validate from "./components/Validation";
+import React, { Component } from "react";
 
-function App() {
-	return (
-		<div>
-			<h2>HELLO WORLD</h2>
-		</div>
-	);
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Validate>
+					<form action="/login" method="post">
+						<div>
+							<label>Username:</label>
+							<input type="text" name="username" />
+						</div>
+						<div>
+							<label>Password:</label>
+							<input type="password" name="password" />
+						</div>
+						<div>
+							<input type="submit" value="Log In" />
+						</div>
+					</form>
+				</Validate>
+			</div>
+		);
+	}
 }
 
 export default App;
