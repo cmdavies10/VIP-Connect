4732f06d4c84ee7a3272125ac7d6a286fdf2e187
Gitana Brazlauskas <Bgitana@hotmail.com>
2019-12-05 15:26:56 -0500

    Does this work?

--- src/App.js
+++ src/App.js
@@ -39,9 +39,9 @@ class App extends React.Component {
 		return (
 			<div className="App">
 				{/* <HomePage />   */}
-
-				<NavBar />
-
+       
+				  <NavBar />
+      
 				<MainTitle />
 				<LogInForm />
 				<ButtonCreate />
