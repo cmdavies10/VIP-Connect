7f513413ce426e4c259dfdc781710b2d9eee0a29
Gitana Brazlauskas <Bgitana@hotmail.com>
2019-12-08 04:07:16 -0500

    created a new logo and polished log in page

--- src/Pages/LogInPage/index.js
+++ src/Pages/LogInPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.css'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+// import Jumbotron from 'react-bootstrap/Jumbotron'
 import Navbar from 'react-bootstrap/Navbar'
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
@@ -10,54 +10,65 @@ import Col from 'react-bootstrap/Col'
 // import NavBar from '../components/NavBar'
 
 
+
 export default function LogInPage (){
     return (
-        <div class="container1" id="tinted-image">
-          {/* <Jumbotron fluid> */}
-          <div class="container2">
-            <Container>
-              <Navbar expand="lg" id="Navbar">
-                <Navbar.Brand href="#home">
-                <div id="logo">VIP<span id='secondLogo'>fan</span></div>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link class="nav-link" href="#link">See Events</Nav.Link>
-                    <Nav.Link href="#link">Buy</Nav.Link>
-                    <Nav.Link href="#link">Create</Nav.Link>
-                    <Nav.Link href="#link">My Tickets</Nav.Link>
-                  </Nav>
-                </Navbar.Collapse>
-              </Navbar>
-              {/* <h1>Fluid jumbotron</h1> */}
-              <p>
-                {/* This is a modified jumbotron that occupies the entire horizontal space of
-                its parent. */}
-              </p>
-          {/* ///////////////////////////////////// */}
-              <Form>
-                <Form.Row>
-                  <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>Email</Form.Label>
+
+    <div class="container-background" id="tinted-image">
+       {/* /* ------------------logo-------------------------- */ }
+       <div class="logo-container">
+        <h6 class="grow">VIPFAN</h6>
+      </div>
+
+          
+       <Container>    
+          {/* /-------------log in form ------------------ */}
+          <div class="container-log-form">
+            <Form>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label class="label">Email</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" class="form-control" />
-                  </Form.Group>
+                </Form.Group>
                   <Form.Group as={Col} controlId="formGridPassword">
-                    <Form.Label type="text" >Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" class="form-control" id="exampleInputPassword1"/>
-                  </Form.Group>
-                </Form.Row>
-                <Button variant="primary" type="submit" btn="rounded">
+                    <Form.Label class="label">Password</Form.Label>
+                      <Form.Control type="password" placeholder="Password" class="form-control" id="exampleInputPassword1"/>
+             </Form.Group>
+              </Form.Row>
+                <Button variant="primary" type="submit" btn="rounded grow">
                   Submit
-                </Button>
-                <Button variant="primary" type="submit" btn="rounded">
+                 </Button>
+
+                 {/* <div class="button_cont"><a class="example_e" href="add-website-here" target="_blank" rel="nofollow noopener">Add Call to Action</a></div> */}
+                 
+                 <Button variant="primary" type="submit" btn="rounded grow">
                   Create
                 </Button>
-              </Form>
-            </Container>
-          {/* </Jumbotron> */}
+            </Form>
+           </div>
+
+           </Container>
+              {/* ---------------- navbar botom---------------------*/}
+          {/* <Navbar sticky="bottom" /> */}
+              <Navbar collapseOnSelect expand="lg"  variant="dark" sticky="bottom">
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+               <Nav className="mr-auto">
+               <Nav.Link href="#features">Home</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link class="text" href="#pricing">See Events</Nav.Link>
+              <Nav.Link href="#features">Buy</Nav.Link>
+              <Nav.Link href="#features">Create</Nav.Link>
+              <Nav.Link href="#pricing">My Tickets</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+
+            
+       
       </div>
-      </div>
+     
+      
     )
 }
--- src/Pages/OptionsPage/index.js
+++ src/Pages/OptionsPage/index.js
@@ -3,28 +3,102 @@ import './index.css'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 export default function OptionsPage (){
     return (
-      <div>
-       <Container>
-        <h1>Make YOur Choise and meet the Band!</h1>
-        <p>
-        Make YOur Choise and meet the Band!
-        </p>
-      </Container>
-            <Card style={{ width: '18rem' }}>
-             <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                 Some Text here...
-                </Card.Text>
-                 <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-                            
+
+      <div class="container-background-none" id="tinted-image">
+      {/* /* ------------------logo-------------------------- */ }
+      <div class="logo-container">
+       <h6 class="grow">VIPFAN</h6>
+     </div>
+
+ {/* /* ------------------page titles-------------------------- */ }
+   <Container>
+      <div class="title-form">  
+         <h1>Make YOur Choise and meet the Band!</h1>
+          <p>
+            Make YOur Choise and meet the Band!
+          </p>
        </div>
+            
+{/* //////////////////////cards///////////////////////// */}
+<Card style={{ width: '18rem' }}>
+  {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+  <Card.Img variant="top" src="https://media.giphy.com/media/l1J9JFmNcyVX8FzP2/giphy.gif" />
+
+ 
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+{/* //////////////////////cards///////////////////////// */}
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/concert-poster-design-template-bb8a458084aec92f6a8ae1874d159dfd_screen.jpg?ts=1566602221" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+{/* //////////////////////cards///////////////////////// */}
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/black-pastel-music-festival-poster-design-template-272b07826d110a9fbf155e44c09cd95b.jpg?ts=1566606312" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+{/* //////////////////////cards///////////////////////// */}
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://media.giphy.com/media/TLTt3x4T67fu5vXAbw/giphy.gif" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+</Container>
+{/* ---------------- navbar botom---------------------*/}
+          {/* <Navbar sticky="bottom" /> */}
+          <Navbar collapseOnSelect expand="lg"  variant="dark" sticky="bottom">
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+               <Nav className="mr-auto">
+               <Nav.Link href="#features">Home</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link class="text" href="#pricing">See Events</Nav.Link>
+              <Nav.Link href="#features">Buy</Nav.Link>
+              <Nav.Link href="#features">Create</Nav.Link>
+              <Nav.Link href="#pricing">My Tickets</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+      
+
+                            
+ </div>
     )
 }
 
