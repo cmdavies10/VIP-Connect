4c26e1b3136c22d9714e73e885a438966ad24331
Chris Davies <c.m.davies10@gmail.com>
2019-12-10 16:42:49 -0500

    viewing build folder

---
+++ build/precache-manifest.11646929e2e998fc73eb1b4a121aed36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1251a088c518b2c5f3a3ddbde2ec639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d1e2da71dcf737053cb",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e0477bfd0ede862f47c3",
+    "url": "/static/css/main.d60ab113.chunk.css"
+  },
+  {
+    "revision": "2d1e2da71dcf737053cb",
+    "url": "/static/js/2.3292e4f7.chunk.js"
+  },
+  {
+    "revision": "e0477bfd0ede862f47c3",
+    "url": "/static/js/main.22bb4dab.chunk.js"
+  },
+  {
+    "revision": "57418a07c3b40cc7fe37",
+    "url": "/static/js/runtime-main.6aca8439.js"
+  },
+  {
+    "revision": "fce883f845a05fce65656ce36ddfb7a8",
+    "url": "/static/media/concert-336695_1280.fce883f8.jpg"
+  },
+  {
+    "revision": "1ac60d197b3680846acb2f0cdc72b368",
+    "url": "/static/media/giphy.1ac60d19.gif"
+  }
+]);
