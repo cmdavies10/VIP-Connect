46cff9c47f97ec39a28f77de8754717e6df24197
Chris Davies <c.m.davies10@gmail.com>
2019-12-10 10:13:23 -0500

    migration to configJS using dotenv

--- config/config.js
+++ config/config.js
@@ -6,5 +6,21 @@ module.exports = {
 		host: process.env.DB_HOST,
 		dialect: "mysql",
 		operatorAliases: false
+	},
+	test: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		dialect: "mysql",
+		operatorAliases: false
+	},
+	production: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		dialect: "mysql",
+		operatorAliases: false
 	}
 };
