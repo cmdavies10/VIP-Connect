dbbfca49c5fbd8363705112ab6f26e3df2b77046
Chris Davies <c.m.davies10@gmail.com>
2019-12-11 05:02:25 -0500

    password auth functional

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
+var passport = require("passport"),
+	LocalStrategy = require("passport-local").Strategy;
 
 var db = require("../models");
 
@@ -19,11 +19,11 @@ passport.use(
 				});
 			}
 			// If there is a user with the given username, but the password the user gives us is incorrect
-			// else if (!dbUser.validPassword(password)) {
-			// 	return done(null, false, {
-			// 		message: "Incorrect password."
-			// 	});
-			// }
+			else if (dbUser.password !== password) {
+				return done(null, false, {
+					message: "Incorrect password."
+				});
+			}
 			// If none of the above, return the user
 			return done(null, dbUser);
 		});
