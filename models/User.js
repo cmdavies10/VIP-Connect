34e94b650a37329e379b25b80efbf4c45fe40643
Chris Davies <c.m.davies10@gmail.com>
2019-12-11 06:08:56 -0500

    googleId

--- models/User.js
+++ models/User.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.STRING,
 			allowNull: false
 		},
-		googleID: DataTypes.STRING
+		googleId: DataTypes.STRING
 	});
 	return User;
 };
